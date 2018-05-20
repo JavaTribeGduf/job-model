b29b794c49a1a628156e1b02427b9d26d6e1dc1e
dk <dkplus@qq.com>
2018-05-20 15:52:53 +0800

    feat: 新增前端内容

---
+++ README.md
@@ -0,0 +1,5 @@
+# 爪哇部落job-model
+
+> 爪哇部落的成员对应画像与技术水平的参考
+
+![前端job-model](https://javatribegduf.github.io/job-model/front-end/index.html)
---
+++ front-end/index.js
@@ -0,0 +1,11 @@
+console.log('%c 欢迎加入爪哇部落前端：',`
+color: #777;
+`);
+console.log(`%c javatribe-fe`,`
+color: #fff;
+background: #777;
+border-radius: 50px 0 50px 0;
+padding: 0 10px;
+border: 1px solid #fff;
+font-size: 20px;
+`);
